1097ecc863e56839f96381cfd2ea544513478e9a
loimax <blazymaxence2018@gmail.com>
2022-07-10 18:01:32 +0200

    jsuis débile

--- LePetageDeCable/gameornot/gameornot.bat
+++ LePetageDeCable/gameornot/gameornot.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
-for /r C:\Users\33652\Desktop\ %%a in (*) do if "%%~nxa"=="gameorno.py" set p=%%~dpnxa
+for /r C:\Users\33652\Desktop\ %%a in (*) do if "%%~nxa"=="gameornot.py" set p=%%~dpnxa
 IF defined p (
     python %p%
     ping 1.1.1.1 -n 5 > nul
