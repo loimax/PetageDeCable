14282aee987aea775a6bb0659c8394aeeef29700
loimax <blazymaxence2018@gmail.com>
2022-07-12 01:17:31 +0200

    plus beau

--- LePetageDeCable/gameornot/gameornot.py
+++ LePetageDeCable/gameornot/gameornot.py
@@ -1,15 +1,13 @@
-from decimal import Decimal
 import random
-
 e = random.randint(0, 1)
 manches = int(input("Combien de manches pour le BO ? "))
 assert manches%2 != 0, "Un BO se joue avec un nombre de manches impaire sans s frère"
 match manches:
     case 1:
         if e == 0:
-            print("On joue bande de pétasses")
+            print(f"\n\n-------------------------------------------\n\t Jouer : 1 - Dormir : 0 \n\tOn joue bande de pétasses\n-------------------------------------------\n")
         else:
-            print("Go dormir bande de pétasses")
+            print(f"\n\n-------------------------------------------\n\t Dormir : 1 - Jouer : 0 \n\tGo dormir bande de pétasses\n-------------------------------------------\n")
     case _:
         dormir = 0
         jouer = 0
@@ -21,7 +19,6 @@ match manches:
                 jouer += 1
             else:
                 dormir += 1
-            # print("Jouer =", jouer, " Dormir = ", dormir)
         if jouer > dormir:
             print(f"\n\n-------------------------------------------\n\t Jouer : {jouer} - Dormir : {dormir} \n\tOn joue bande de pétasses\n-------------------------------------------\n")
         else:
